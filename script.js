317d9558eddca32006c7d8dd3d410161e8eeb6a7
Orlando Ashley <orlando.ashley@gmail.com>
2021-03-03 00:48:56 -0500

    created number and symbol functions

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//generator functions
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbol ='@#()<>*$%&^!'
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+//console logging function
+console.log(getRandomSymbol());
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  let password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
